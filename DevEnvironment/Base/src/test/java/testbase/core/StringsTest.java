31d67869eeb7de0d0fdf66ad445306d677a5f92f
Bental Tagor <bentaltagor@yahoo.com>
2019-10-27 21:37:37 -0700

    Adding a unit test for Strings.

---
+++ DevEnvironment/Base/src/test/java/testbase/core/StringsTest.java
@@ -0,0 +1,68 @@
+package testbase.core;
+
+import base.core.ArrayIterator;
+import base.core.Strings;
+import base.interfaces.IIterator;
+import base.interfaces.ITestData;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import testbase.interfaces.IAssertion;
+
+/**
+ * The StringsTest class implements tests for strings.
+ */
+public final class StringsTest {
+    private final IAssertion assertion = new Assertion();
+    private final ITestData testData = new TestData();
+
+    /**
+     * The StringsTest constructor.
+     */
+    public StringsTest() {
+    }
+
+    /**
+     * Initialize the tests.
+     */
+    @Before
+    public void setUp() {
+    }
+
+    /**
+     * Un-Initializes the tests.
+     */
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Tests the logic whether a string is null or empty.
+     */
+    @Test
+    public void isNullOrEmptyTest() {
+        String[] strings = {"abc", "", null};
+        Boolean[] expectedResults = {false, true, true};
+
+        IIterator<String> stringIterator = ArrayIterator.of(strings);
+        IIterator<Boolean> expectedResultIterator = ArrayIterator.of(expectedResults);
+
+        while (stringIterator.hasNext() && expectedResultIterator.hasNext()) {
+            String value = stringIterator.next();
+            boolean expectedResult = expectedResultIterator.next();
+
+            this.testStringIsNullOrEmpty(value, expectedResult);
+        }
+    }
+
+    /**
+     * Tests the logic whether a string is null or empty.
+     */
+    private void testStringIsNullOrEmpty(String value, boolean expectedResult) {
+        boolean result = Strings.isNullOrEmpty(value);
+
+        this.assertion.assertEquals(
+            result,
+            expectedResult, "Invalid logic for determining whether a string is null or empty.");
+    }
+}
