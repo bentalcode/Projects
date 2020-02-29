dd92f75915501454b25bc44139c297b1695b12a7
Bental Tagor <bentalt@logic2020.com>
2020-02-28 23:40:39 -0800

    Implementing the framework of the DataTransformationService by using spring Spring Initializr.

---
+++ DevEnvironment/DataTransformationService/src/main/groovy/com/datatransformation/DataTransformationService/DataTransformationServiceApplication.groovy
@@ -0,0 +1,13 @@
+package com.datatransformation.DataTransformationService
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DataTransformationServiceApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DataTransformationServiceApplication, args)
+	}
+
+}
---
+++ DevEnvironment/DataTransformationService/src/test/groovy/com/datatransformation/DataTransformationService/DataTransformationServiceApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.datatransformation.DataTransformationService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataTransformationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
