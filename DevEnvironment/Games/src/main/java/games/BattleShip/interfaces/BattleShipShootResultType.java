2db6be7b4c4e63fd1941152696e81f1e6ccd950f
Bental Tagor <bentaltagor@yahoo.com>
2020-08-09 18:19:50 -0700

    Implementing a board of battle ships.

---
+++ DevEnvironment/Games/src/main/java/games/BattleShip/interfaces/BattleShipShootResultType.java
@@ -0,0 +1,10 @@
+package games.BattleShip.interfaces;
+
+/**
+ * The BattleShipShootResultType defines the shoot result of a battle ship.
+ */
+public enum BattleShipShootResultType {
+    Hit,
+    Miss,
+    Sunk
+}
