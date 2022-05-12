c1457f32f5f8cc85e5b60d261a9ca55f73a61021
Bental Tagor <bentaltagor@protonmail.com>
2022-05-12 09:30:35 -0700

    Defining a generic test.

---
+++ DevEnvironment/Problems/src/test/java/problems/core/GenericTest.java
@@ -0,0 +1,18 @@
+package problems.core;
+
+import org.junit.Test;
+
+public final class GenericTest {
+    /**
+     * The GenericTest constructor.
+     */
+    public GenericTest() {
+    }
+
+    /**
+     * Tests the logic of the generic test.
+     */
+    @Test
+    public void test() {
+    }
+}
